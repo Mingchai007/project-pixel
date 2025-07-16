78eaabcbb438b49a61e937be99a8566b14bfbb28
Mingchai007 <79045797+Mingchai007@users.noreply.github.com>
2025-07-16 16:42:49 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 👇 สำคัญ! บังคับให้ export เป็น Static
+  output: "export",
+
+  // 👇 ปรับ basePath และ assetPrefix ให้ตรงกับชื่อ repo
+  basePath: isProd ? "/project-pixel" : "",
+  assetPrefix: isProd ? "/project-pixel/" : "",
+
+  // ถ้าใช้ image ของ next/image ควรเพิ่ม domains หรือ unoptimized
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
